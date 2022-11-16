bfae1ed25a20517b7a2f48774a16231fc8da54b8
Plum <pplluumm@users.noreply.github.com>
2022-11-15 16:42:40 -0800

    button onclicks

--- justdoit/script_justdoit.js
+++ justdoit/script_justdoit.js
@@ -1,5 +1,29 @@
 
 
-function buttonStart() {
-  alert("Start");
+function buttonStart() 
+{
+  document.getElementById("section_start").style.display = "none";
+  document.getElementById("section_goal").style.display = "";
+}
+
+function buttonSubmitGoal()
+{
+  alert("buttonSubmitGoal");
+}
+
+function buttonNextMindfulness()
+{
+  alert("buttonNextMindfulness");
+}
+
+function buttonsubmitObjective()
+{
+  alert("buttonsubmitObjective");
+
+}
+
+function buttonNextDone()
+{
+  alert("buttonNextDone");
+
 }
