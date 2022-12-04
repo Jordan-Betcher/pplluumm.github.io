04620c1af3d43fb6f001488304e498a45142c6db
Plum <pplluumm@users.noreply.github.com>
2022-12-03 19:03:29 -0800

    Center Top, parchment style vis css, button swaps between questions

--- justdoit/script_justdoit.js
+++ justdoit/script_justdoit.js
@@ -8,17 +8,22 @@ function buttonStart()
 
 function buttonSubmitGoal()
 {
-  alert("buttonSubmitGoal");
+  document.getElementById("section_goal").style.display = "none";
+  document.getElementById("section_mindfulness").style.display = "";
+  let goal = document.getElementById("textarea_goal").value;
+  document.getElementById("question_create_objective").textContent = "What is the next tactile step towards " + goal + " that is under a minute";
 }
 
 function buttonNextMindfulness()
 {
-  alert("buttonNextMindfulness");
+  document.getElementById("section_mindfulness").style.display = "none";
+  document.getElementById("section_objective").style.display = "";
 }
 
 function buttonsubmitObjective()
 {
-  alert("buttonsubmitObjective");
+  document.getElementById("section_objective").style.display = "none";
+  document.getElementById("section_done").style.display = "";
 
 }
 
